12e7ba57e9d8659d4c1a9228ac4b5e2afc2dbea2
Anders <anders@andersnoren.se>
2019-09-07 11:38:39 +0200

    Added the footer structure and styling from the design
    Updated the footer structure to match the structure used in the design. Also includes styles for the social icons and base widget styles (widget specific styles are still needed).

--- footer.php
+++ footer.php
@@ -1,69 +1,117 @@
 		<footer id="site-footer" role="contentinfo">
 
-			<?php if ( is_active_sidebar( 'footer-one' ) || is_active_sidebar( 'footer-two' ) ) : ?>
+			<div class="footer-inner section-inner">
 
-				<div class="footer-widgets-outer-wrapper section-inner">
-				
-					<div class="footer-widgets-wrapper">
+				<?php
 
-						<?php if ( is_active_sidebar( 'footer-one' ) ) : ?>
-							<div class="footer-widgets column-one grid-item">
-								<?php dynamic_sidebar( 'footer-one' ); ?>
-							</div>
+				$has_footer_menu = has_nav_menu( 'footer-menu' );
+				$has_social_menu = has_nav_menu( 'social-menu' );
+
+				$footer_top_classes = '';
+
+				$footer_top_classes .= $has_footer_menu ? ' has-footer-menu' : '';
+				$footer_top_classes .= $has_social_menu ? ' has-social-menu' : '';
+
+				if ( $has_footer_menu ) : ?>
+
+					<div class="footer-top<?php echo esc_attr( $footer_top_classes ); ?>">
+
+						<ul class="footer-menu reset-list-style">
+							<?php
+							wp_nav_menu( array(
+								'container' 		=> '',
+								'depth'				=> 1,
+								'items_wrap' 		=> '%3$s',
+								'theme_location' 	=> 'footer-menu',
+							) );
+							?>
+						</ul><!-- .site-nav -->
+
+						<?php if ( $has_social_menu ) : ?>
+
+							<div class="footer-social-wrapper">
+
+								<ul class="social-menu footer-social reset-list-style social-icons s-icons">
+
+									<?php
+									wp_nav_menu( array(
+										'theme_location'	=> 'social-menu',
+										'container'			=> '',
+										'container_class'	=> '',
+										'items_wrap'		=> '%3$s',
+										'menu_id'			=> '',
+										'menu_class'		=> '',
+										'depth'				=> 1,
+										'link_before'		=> '<span class="screen-reader-text">',
+										'link_after'		=> '</span>',
+										'fallback_cb'		=> '',
+									) );
+									?>
+
+								</ul><!-- .social-menu -->
+
+							</div><!-- .footer-social-wrapper -->
+
 						<?php endif; ?>
 
-						<?php if ( is_active_sidebar( 'footer-two' ) ) : ?>
-							<div class="footer-widgets column-two grid-item">
-								<?php dynamic_sidebar( 'footer-two' ); ?>
-							</div>
-						<?php endif; ?>
-
-					</div><!-- .footer-widgets-wrapper -->
-					
-				</div><!-- .footer-widgets-outer-wrapper -->
-
-			<?php endif; 
-
-			$has_footer_menu = has_nav_menu( 'footer-menu' );
-
-			$footer_inner_classes = '';
-
-			if ( $has_footer_menu ) {
-				$footer_inner_classes .= ' has-footer-menu';
-			}
-			
-			?>
-
-			<div class="footer-inner section-inner<?php echo esc_attr( $footer_inner_classes ); ?>">
-
-				<?php if ( $has_footer_menu ) : ?>
-
-					<ul class="footer-menu reset-list-style">
-						<?php
-						wp_nav_menu( array(
-							'container' 		=> '',
-							'depth'				=> 1,
-							'items_wrap' 		=> '%3$s',
-							'theme_location' 	=> 'footer-menu',
-						) );
-						?>
-					</ul><!-- .site-nav -->
+					</div><!-- .footer-top -->
 
 				<?php endif; ?>
 
-				<div class="footer-credits">
+				<?php if ( is_active_sidebar( 'footer-one' ) || is_active_sidebar( 'footer-two' ) ) : ?>
 
-					<p class="footer-copyright">&copy; <?php echo esc_html( date_i18n( __( 'Y', 'twentytwenty' ) ) ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php echo bloginfo( 'name' ); ?></a></p>
+					<div class="footer-widgets-outer-wrapper">
+					
+						<div class="footer-widgets-wrapper">
 
-					<p class="theme-credits color-secondary">
-						<?php
-						/* Translators: $s = WordPress */
-						printf( esc_html_x( 'Powered by %s', 'Translators: $s = name of the theme developer', 'twentytwenty' ), '<a href="https://www.wordpress.org">' . esc_html__( 'WordPress', 'twentytwenty' ) . '</a>' ); ?>
-					</p><!-- .theme-credits -->
+							<?php if ( is_active_sidebar( 'footer-one' ) ) : ?>
+								<div class="footer-widgets column-one grid-item">
+									<?php dynamic_sidebar( 'footer-one' ); ?>
+								</div>
+							<?php endif; ?>
 
-				</div><!-- .footer-credits -->
+							<?php if ( is_active_sidebar( 'footer-two' ) ) : ?>
+								<div class="footer-widgets column-two grid-item">
+									<?php dynamic_sidebar( 'footer-two' ); ?>
+								</div>
+							<?php endif; ?>
 
-			</div><!-- .footer-bottom -->
+						</div><!-- .footer-widgets-wrapper -->
+						
+					</div><!-- .footer-widgets-outer-wrapper -->
+
+				<?php endif; ?>
+
+				<div class="footer-bottom">
+
+					<div class="footer-credits">
+
+						<p class="footer-copyright">&copy; <?php echo esc_html( date_i18n( __( 'Y', 'twentytwenty' ) ) ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php echo bloginfo( 'name' ); ?></a></p>
+
+						<p class="powered-by-wordpress">
+							<?php
+							/* Translators: $s = WordPress */
+							printf( esc_html_x( 'Powered by %s', 'Translators: $s = name of the theme developer', 'twentytwenty' ), '<a href="https://www.wordpress.org">' . esc_html__( 'WordPress', 'twentytwenty' ) . '</a>' ); ?>
+						</p><!-- .theme-credits -->
+
+					</div><!-- .footer-credits -->
+
+					<a class="to-the-top" href="#site-header">
+						<span class="to-the-top-long">
+							<?php 
+							// Translators: %s = HTML character for an arrow.
+							printf( _x( 'To the top %s', '%s = HTML character for an arrow', 'twentytwenty' ), '<span class="arrow">&uarr;</span>' ); ?>
+						</span>
+						<span class="to-the-top-short">
+							<?php 
+							// Translators: %s = HTML character for an arrow.
+							printf( _x( 'Up %s', '%s = HTML character for an arrow', 'twentytwenty' ), '<span class="arrow">&uarr;</span>' ); ?>
+						</span>
+					</a>
+
+				</div><!-- .footer-bottom -->
+
+			</div><!-- .footer-inner -->
 
 		</footer><!-- #site-footer -->
 
