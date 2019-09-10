b1e94489bf96101a3b901a805fd6ec49e4944064
William Patton <will@pattonwebz.com>
2019-09-10 20:46:05 +0100

    Fix all WPCS notices in the pagination.php file (#198)
    * Add escape and contextual translation to pagination strings
    
    * WPCS: add comment to indicate output is escaped, add file header.
    
    * Removed endif

--- pagination.php
+++ pagination.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * A template partial to output pagination for the Twenty Twenty default theme.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since 1.0.0
+ */
 
 $posts_pagination = get_the_posts_pagination(
 	array(
@@ -6,22 +15,22 @@ $posts_pagination = get_the_posts_pagination(
 		'prev_text' => sprintf(
 			'%s <span class="nav-prev-text">%s</span>',
 			'&larr;',
-			__( 'Newer Posts', 'twentytwenty' )
+			esc_html( _x( 'Newer Posts', 'previous set of posts' ), 'twentytwenty' )
 		),
 		'next_text' => sprintf(
 			'<span class="nav-next-text">%s</span> %s',
-			__( 'Older Posts', 'twentytwenty' ),
+			esc_html( _x( 'Older Posts', 'next set of posts' ), 'twentytwenty' ),
 			'&rarr;'
 		),
 	)
 );
 
-if ( $posts_pagination ) : ?>
+if ( $posts_pagination ) { ?>
 
 	<div class="pagination-wrapper section-inner">
 
-		<?php echo $posts_pagination; ?>
+		<?php echo $posts_pagination; //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- already escaped during generation. ?>
 
 	</div><!-- .pagination-wrapper -->
 
-<?php endif; ?>
+<?php } ?>
