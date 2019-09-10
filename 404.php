2ef1c0e512a3a0d9613d179db4ad61288752c4e8
Juanfra Aldasoro <juanfraa@gmail.com>
2019-09-10 10:48:23 +0200

    Remove escaping.

--- 404.php
+++ 404.php
@@ -4,9 +4,9 @@
 
 	<div class="section-inner thin">
 
-		<h1 class="archive-title"><?php esc_html_e( 'Page Not Found', 'twentytwenty' ); ?></h1>
+		<h1 class="archive-title"><?php _e( 'Page Not Found', 'twentytwenty' ); ?></h1>
 			
-		<div class="intro-text archive-subtitle"><p><?php esc_html_e( 'The page you were looking for could not be found. It might have been removed, renamed, or did not exist in the first place.', 'twentytwenty' ); ?></p></div>
+		<div class="intro-text archive-subtitle"><p><?php _e( 'The page you were looking for could not be found. It might have been removed, renamed, or did not exist in the first place.', 'twentytwenty' ); ?></p></div>
 
 		<?php get_search_form(); ?>
 
