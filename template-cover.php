fa9265d16bed263fe10754c53dedaee6a72a7173
Ian <belangeri79@gmail.com>
2019-09-10 14:59:24 -0400

    Update PR

--- template-cover.php
+++ template-cover.php
@@ -2,6 +2,10 @@
 /**
  * Template Name: Cover Template
  * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since 1.0
  */
 
 get_template_part( 'singular' );
