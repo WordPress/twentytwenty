fe81d812bcbfb27daad19680d7188cf86215a808
Acosmin <acosmin@users.noreply.github.com>
2019-09-11 10:42:09 +0300

    Removed double `get_theme_mod`

--- header.php
+++ header.php
@@ -42,7 +42,7 @@
 					// Check whether the header search is deactivated in the customizer.
 					$disable_header_search = get_theme_mod( 'twentytwenty_disable_header_search', false );
 
-					if( get_theme_mod( $disable_header_search ) ) {
+					if( ! $disable_header_search ) {
 					?>
 					
 						<button class="toggle search-toggle mobile-search-toggle" data-toggle-target=".search-modal" data-toggle-screen-lock="true" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
