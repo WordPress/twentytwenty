983ccfaad04a94f4ca0a6f573aae54a0ea169a4d
Acosmin <acosmin@users.noreply.github.com>
2019-09-14 15:25:26 +0300

    Implemented the Image Overlay Text Color option (#245)
    * Cover font color
    * Added post meta and excerpt back

--- inc/custom-css.php
+++ inc/custom-css.php
@@ -53,8 +53,12 @@ if ( ! function_exists( 'twentytwenty_get_customizer_css' ) ) {
 
 		// Get variables.
 		$accent          = sanitize_hex_color( get_theme_mod( 'twentytwenty_accent_color' ) );
-		$accent_default  = '#CD2653';
+		$accent_default  = '#cd2653';
 		$buttons_targets = apply_filters( 'twentytwenty_buttons_targets_front_end', 'button, .button, .faux-button, .wp-block-button__link, .wp-block-file__button, input[type=\'button\'], input[type=\'reset\'], input[type=\'submit\']' );
+
+		// Cover.
+		$cover         = sanitize_hex_color( get_theme_mod( 'twentytwenty_cover_template_overlay_text_color' ) );
+		$cover_default = '#ffffff';
 
 		// Background.
 		$background         = sanitize_hex_color_no_hash( get_theme_mod( 'background_color' ) );
@@ -82,6 +86,10 @@ if ( ! function_exists( 'twentytwenty_get_customizer_css' ) ) {
 				twentytwenty_generate_css( 'blockquote, .wp-block-button.is-style-outline', 'border-color', $accent );
 				twentytwenty_generate_css( $buttons_targets, 'background-color', $accent );
 				twentytwenty_generate_css( '.footer-social a, .social-icons a', 'background-color', $accent );
+			}
+
+			if ( $cover && $cover !== $cover_default ) {
+				twentytwenty_generate_css( '.cover-header .entry-header *', 'color', $cover );
 			}
 
 			// Helper Classes.
