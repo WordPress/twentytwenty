b7bf49f8ea37288fa88f8ba6ef3bfae21ddd24ea
Charlie <carlos@patata.io>
2019-09-15 21:15:29 +0200

    #8 Move page links outside content area

--- template-parts/content.php
+++ template-parts/content.php
@@ -34,17 +34,21 @@
 				the_content();
 			}
 
-			wp_link_pages(
-				array(
-					'before' => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
-					'after'  => '</nav>',
-				)
-			);
-
 			edit_post_link();
 			?>
 
 		</div><!-- .entry-content -->
+
+        <div class="link-pages">
+            <?php
+            wp_link_pages(
+                array(
+                    'before' => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
+                    'after'  => '</nav>',
+                )
+            );
+            ?>
+        </div><!-- .link-pages -->
 
 		<?php
 		// Single bottom post meta.
