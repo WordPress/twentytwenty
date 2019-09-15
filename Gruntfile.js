259ba92a30c57f4e55becf65e7fede642ff27f7b
Joost de Valk <joost@yoast.com>
2019-09-15 13:23:59 +0200

    Confifugre PostCSS better

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,39 @@
 /* global require, process */
 module.exports = function ( grunt ) {
-	grunt.loadNpmTasks( 'grunt-contrib-copy' );
 	grunt.loadNpmTasks( 'grunt-postcss' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 	grunt.registerTask( 'build:js', ['uglify'] );
-	grunt.registerTask( 'build:css', ['copy:css', 'postcss'] );
+	grunt.registerTask( 'build:css', ['postcss'] );
 	grunt.registerTask( 'build', ['build:js', 'build:css'] );
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' ),
-		copy: {
-			css: {
-				files: [
-					{src: 'style.css', dest: 'assets/dist/css/style.min.css'},
-					{src: 'assets/css/font-awesome.css', dest: 'assets/dist/css/font-awesome.min.css'},
-				]
-			}
-		},
 		postcss: {
 			options: {
 				map: {
 					inline: false, // save all sourcemaps as separate files...
 				},
 				processors: [
+					require( 'postcss-url' )({
+						url: "rebase"
+					}),
 					require( 'autoprefixer' )(), // add vendor prefixes
 					require( 'cssnano' )() // minify the result
 				]
 			},
-			dist: {
-				src: ['assets/dist/css/*.min.css']
-			}
+			dist:
+				{
+					files: [
+						{
+							src: 'style.css',
+							dest: 'assets/dist/css/style.min.css'
+						},
+						{
+							src: 'assets/css/font-awesome.css',
+							dest: 'assets/dist/css/font-awesome.min.css'
+						}
+					]
+				}
 		},
 		uglify: {
 			construct: {
