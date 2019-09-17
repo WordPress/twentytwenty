9bf267a78a6c1f3dcac51f939e9a1a8c2e0ed8ae
Acosmin <acosmin@users.noreply.github.com>
2019-09-17 09:00:05 +0300

    Fix for twentytwenty_read_more_tag()

--- functions.php
+++ functions.php
@@ -506,8 +506,7 @@ if ( ! function_exists( 'twentytwenty_read_more_tag' ) ) {
 		return sprintf(
 			'<a href="%1$s" class="more-link">%2$s <span class="screen-reader-text">"%3$s"</span></a></p>',
 			esc_url( get_permalink( get_the_ID() ) ),
-			/* Translators: %s: Name of current post */
-			esc_html( 'Continue reading', 'twentytwenty' ),
+			esc_html__( 'Continue reading', 'twentytwenty' ),
 			get_the_title( get_the_ID() )
 		);
 	}
