618743d0f3eca0d772c3d9c0fa07a9e4f329ae31
Anders <anders@andersnoren.se>
2019-09-17 22:30:12 +0200

    Added category output to entry header, updated styling on intro-text on singular

--- template-parts/content-cover.php
+++ template-parts/content-cover.php
@@ -56,9 +56,22 @@
 					<header class="entry-header has-text-align-center">
 						<div class="entry-header-inner section-inner medium">
 
-							<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
 							<?php
+
+							if ( has_category() ) {
+								?>
+
+								<div class="entry-categories">
+									<div class="entry-categories-inner">
+										<?php the_category( ' ' ); ?>
+									</div><!-- .entry-categories-inner -->
+								</div><!-- .entry-categories -->
+
+								<?php
+							}
+
+							the_title( '<h1 class="entry-title">', '</h1>' );
+							
 							if ( is_page() ) {
 								?>
 
@@ -71,15 +84,31 @@
 
 								</div><!-- .to-the-content-wrapper -->
 
-							<?php } else { ?>
+								<?php 
+							} else {
 
-								<div class="intro-text section-inner thin max-percentage">
-									<?php the_excerpt(); ?>
-								</div>
+								$intro_text_width = '';
 
-								<?php twentytwenty_the_post_meta( get_the_ID(), 'single-top' ); ?>
+								if ( is_singular() ) {
+									$intro_text_width = ' small';
+								} else {
+									$intro_text_width = ' thin';
+								}
 
-							<?php } ?>
+								if ( has_excerpt() ) {
+									?>
+
+									<div class="intro-text section-inner max-percentage<?php echo esc_attr( $intro_text_width ); ?>">
+										<?php the_excerpt(); ?>
+									</div>
+
+									<?php
+								}
+
+								twentytwenty_the_post_meta( get_the_ID(), 'single-top' ); 
+								
+							}
+							?>
 
 						</div><!-- .entry-header-inner -->
 					</header><!-- .entry-header -->
