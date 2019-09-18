a1c38e9847bff4672f87e820c52eb97ac0c0e8d0
Fabian Kägy <fabian@arvernus.info>
2019-09-18 12:01:22 +0200

    add forEach polyfill if not already implemented

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,17 @@
 
 var twentytwenty = twentytwenty || {}
 
+// polyfill forEach
+// https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill
+if (window.NodeList && !NodeList.prototype.forEach) {
+	NodeList.prototype.forEach = function (callback, thisArg) {
+		thisArg = thisArg || window;
+		for (var i = 0; i < this.length; i++) {
+			callback.call(thisArg, this[i], i, this);
+		}
+	};
+}
+
 /*	-----------------------------------------------------------------------------------------------
 	Cover Modals
 --------------------------------------------------------------------------------------------------- */
