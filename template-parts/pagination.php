ca63616e473b54e4a658dd4b45b5f571b33bc51f
anace <anaclaudia.alfieri@gmail.com>
2019-09-18 20:52:59 +0200

    reverting site pagination to original

--- template-parts/pagination.php
+++ template-parts/pagination.php
@@ -9,8 +9,6 @@
  * @since 1.0.0
  */
 
-/* Delete from here */
-
 $posts_pagination = get_the_posts_pagination(
 	array(
 		'mid_size'  => 2,
@@ -49,41 +47,5 @@ if ( $posts_pagination ) { ?>
 
 	</div><!-- .pagination-wrapper -->
 
-
-
 	<?php
 }
-
-/* Delete until here */
-
-/* Alternative 1 */
-
-the_posts_pagination(
-	array(
-		'mid_size'           => 2,
-		/**
-		 * Translators:
-		 * This text contains HTML to allow the text to be shorter on small screens.
-		 * The text inside the span with the class nav-short will be hidden on small screens.
-		 */
-		'prev_text'          => '<span class="arrow" aria-hidden="true">&larr; </span><span class="nav-text">' . __( 'Newer <span class="nav-short">Posts</span>', 'twentytwenty' ) . '</span>',
-		'next_text'          => '<span class="nav-text">' . __( 'Older <span class="nav-short">Posts</span>', 'twentytwenty' ) . '</span><span class="arrow" aria-hidden="true"> &rarr;</span>',
-		'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentytwenty' ) . ' </span>',
-	)
-);
-
-/* Alternative 2 */
-
-the_posts_pagination(
-	array(
-		'mid_size'           => 2,
-		/**
-		 * Translators:
-		 * This text contains HTML to allow the text to be shorter on small screens.
-		 * The text inside the span with the class nav-short will be hidden on small screens.
-		 */
-		'prev_text'          => sprintf( '<span class="arrow" aria-hidden="true">%1$s</span><span class="nav-text">%2$s</span>', '&larr; ', __( 'Newer <span class="nav-short">Posts</span>', 'twentytwenty' ) ),
-		'next_text'          => sprintf( '<span class="nav-text">%1$s</span><span class="arrow" aria-hidden="true">%2$s</span>', __( 'Older <span class="nav-short">Posts</span>', 'twentytwenty' ), ' &rarr;' ),
-		'before_page_number' => sprintf( '<span class="meta-nav screen-reader-text">%s</span>', __( 'Page', 'twentytwenty' ) ),
-	)
-);
