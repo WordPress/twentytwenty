78691ec6490a87120d03212270dda05e576902ab
anace <anaclaudia.alfieri@gmail.com>
2019-09-19 01:53:36 +0200

    modified site pagination markup and css

--- template-parts/pagination.php
+++ template-parts/pagination.php
@@ -9,43 +9,16 @@
  * @since 1.0.0
  */
 
-$posts_pagination = get_the_posts_pagination(
+the_posts_pagination(
 	array(
-		'mid_size'  => 2,
+		'mid_size'           => 2,
 		/**
 		 * Translators:
-		* This text contains HTML to allow the text to be shorter on small screens.
-		* The text inside the span with the class nav-short will be hidden on small screens.
-		*/
-		'prev_text' => sprintf(
-			'%s <span class="nav-prev-text">%s</span>',
-			'&larr;',
-			__( 'Newer <span class="nav-short">Posts</span>', 'twentytwenty' )
-		),
-		'next_text' => sprintf(
-			'<span class="nav-next-text">%s</span> %s',
-			__( 'Older <span class="nav-short">Posts</span>', 'twentytwenty' ),
-			'&rarr;'
-		),
+		 * This text contains HTML to allow the text to be shorter on small screens.
+		 * The text inside the span with the class nav-short will be hidden on small screens.
+		 */
+		'prev_text'          => sprintf( '<span class="arrow" aria-hidden="true">%1$s</span><span class="nav-text">%2$s</span>', '&larr; ', __( 'Newer <span class="nav-short">Posts</span>', 'twentytwenty' ) ),
+		'next_text'          => sprintf( '<span class="nav-text">%1$s</span><span class="arrow" aria-hidden="true">%2$s</span>', __( 'Older <span class="nav-short">Posts</span>', 'twentytwenty' ), ' &rarr;' ),
+		'before_page_number' => sprintf( '<span class="meta-nav screen-reader-text">%s</span>', __( 'Page', 'twentytwenty' ) ),
 	)
 );
-
-// If we're only showing one of the next or previous links, add a class indicating so.
-if ( strpos( $posts_pagination, 'prev page-numbers' ) === false ) {
-	$pagination_classes = ' only-next';
-} elseif ( strpos( $posts_pagination, 'next page-numbers' ) === false ) {
-	$pagination_classes = ' only-prev';
-} else {
-	$pagination_classes = '';
-}
-
-if ( $posts_pagination ) { ?>
-
-	<div class="pagination-wrapper section-inner">
-
-		<?php echo $posts_pagination; //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- already escaped during generation. ?>
-
-	</div><!-- .pagination-wrapper -->
-
-	<?php
-}
