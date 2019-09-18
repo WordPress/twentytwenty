ef7d552265443da50b9f0eeaf61c95c6562b2072
Niels Lange <info@nielslange.de>
2019-09-18 19:58:07 +0200

    UPDATE #344: The `Continue Reading` button should be centered in index view

--- functions.php
+++ functions.php
@@ -506,7 +506,7 @@ if ( ! function_exists( 'twentytwenty_read_more_tag' ) ) {
 	 */
 	function twentytwenty_read_more_tag() {
 		return sprintf(
-			'<a href="%1$s" class="more-link faux-button">%2$s <span class="screen-reader-text">"%3$s"</span></a>',
+			'<p class="has-text-align-center"><a href="%1$s" class="more-link faux-button">%2$s <span class="screen-reader-text">"%3$s"</span></a></p>',
 			esc_url( get_permalink( get_the_ID() ) ),
 			esc_html__( 'Continue reading', 'twentytwenty' ),
 			get_the_title( get_the_ID() )
