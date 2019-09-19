f52510495c96f41a6e563b0561f27629b011e147
Acosmin <acosmin@users.noreply.github.com>
2019-09-19 09:41:23 +0300

    Fixed title/logo in Customizer

--- classes/class-twentytwenty-customize.php
+++ classes/class-twentytwenty-customize.php
@@ -42,6 +42,14 @@ if ( ! class_exists( 'TwentyTwenty_Customize' ) ) {
 				)
 			);
 
+			$wp_customize->selective_refresh->add_partial(
+				'custom_logo',
+				array(
+					'selector'        => '.header-titles [class*=site-]:not(.site-description)',
+					'render_callback' => 'twentytwenty_site_logo',
+				)
+			);
+
 			/**
 			 * Site Identity
 			 */
