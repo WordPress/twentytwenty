6e7571193eba5357eb206040d4a8ebe9384020dd
anace <anaclaudia.alfieri@gmail.com>
2019-09-19 16:36:05 +0200

    added the class focus to menu items with children

--- assets/js/index.js
+++ assets/js/index.js
@@ -858,3 +858,40 @@ function twentytwentyScrollTo(to, callback, duration) {
 	};
 	animateScroll();
 };
+
+/*	-----------------------------------------------------------------------------------------------
+	Add the 'focus' class to all 'li.menu-item-has-children' when the 'focus' is on the a element.
+--------------------------------------------------------------------------------------------------- */
+
+	// Get all the link elements within the primary menu.
+
+	menu = document.querySelector( '.primary-menu-wrapper nav' );
+	links = menu.getElementsByTagName( 'a' );
+
+	// Each time a menu link is focused or blurred, toggle focus.
+	for ( i = 0, len = links.length; i < len; i++ ) {
+		links[i].addEventListener( 'focus', toggleFocus, true );
+		links[i].addEventListener( 'blur', toggleFocus, true );
+	}
+
+	/**
+	 * Sets or removes .focus class on an element.
+	 */
+	function toggleFocus() {
+		var self = this;
+
+		// Move up through the ancestors of the current link until we hit .primary-menu.
+		while ( -1 === self.className.indexOf( 'primary-menu' ) ) {
+
+			// On li elements toggle the class .focus.
+			if ( 'li' === self.tagName.toLowerCase() ) {
+				
+				if ( -1 !== self.className.indexOf( 'focus' ) ) {
+					self.className = self.className.replace( ' focus', '' );
+				} else {
+					self.className += ' focus';
+				}
+			}
+			self = self.parentElement;
+		}
+	}
