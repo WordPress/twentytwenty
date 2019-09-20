425055e199ea75d4d9caaf16c86719071f7a37b8
Ismail El Korchi <ismail.elkorchi@gmail.com>
2019-09-20 22:40:05 +0100

    remove an unused function

--- assets/js/index.js
+++ assets/js/index.js
@@ -551,18 +551,6 @@ function twentytwentyToggleAttribute( element, attribute, trueVal, falseVal ) {
 	}
 }
 
-function twentytwentyGetQueryStringValue() {
-	const vars = [];
-	let hash;
-	const hashes = window.location.href.slice( window.location.href.indexOf( '?' ) + 1 ).split( '&' );
-	for ( let i = 0; i < hashes.length; i++ ) {
-		hash = hashes[ i ].split( '=' );
-		vars.push( hash[ 0 ] );
-		vars[ hash[ 0 ] ] = hash[ 1 ];
-	}
-	return vars;
-}
-
 /**
  * twentytwentySlideUp
  *
