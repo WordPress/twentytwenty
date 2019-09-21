9aca7704b0f8a365d32b92e4ec038bebc6ccb5d8
Charlie <carlos@patata.io>
2019-09-21 17:30:53 +0200

    #433 - Changes requested done

--- footer.php
+++ footer.php
@@ -26,10 +26,9 @@
 
 				$footer_top_classes .= $has_footer_menu ? ' has-footer-menu' : '';
 				$footer_top_classes .= $has_social_menu ? ' has-social-menu' : '';
+				$footer_social_wrapper_class = $has_footer_menu ? 'footer-social-wrapper' : '';
 
-				if ( $has_footer_menu || $has_social_menu ) {
-
-				?>
+				if ( $has_footer_menu || $has_social_menu ) { ?>
 					<div class="footer-top<?php echo esc_attr( $footer_top_classes ); ?>">
 						<?php if ( $has_footer_menu ) { ?>
 
@@ -39,9 +38,9 @@
 									<?php
 									wp_nav_menu(
 										array(
-											'container'      => '',
-											'depth'          => 1,
-											'items_wrap'     => '%3$s',
+											'container' => '',
+											'depth' => 1,
+											'items_wrap' => '%3$s',
 											'theme_location' => 'footer',
 										)
 									);
@@ -53,7 +52,7 @@
 						<?php } ?>
 						<?php if ( $has_social_menu ) { ?>
 
-							<div class="footer-social-wrapper">
+							<div class="<?php esc_attr( $footer_social_wrapper_class ); ?>">
 
 								<nav aria-label="<?php esc_attr_e( 'Social links', 'twentytwenty' ); ?>">
 
@@ -120,14 +119,14 @@
 
 					<div class="footer-credits">
 
-						<p class="footer-copyright">&copy; 
-							<?php 
-							echo esc_html( 
-								date_i18n( 
+						<p class="footer-copyright">&copy;
+							<?php
+							echo esc_html(
+								date_i18n(
 									/* Translators: Y = Format parameter for date() https://php.net/manual/en/function.date.php */
-									_x( 'Y', 'Translators: Y = Current year', 'twentytwenty' ) 
-								) 
-							); 
+									_x( 'Y', 'Translators: Y = Current year', 'twentytwenty' )
+								)
+							);
 							?>
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo bloginfo( 'name' ); ?></a></a>
 						</p>
