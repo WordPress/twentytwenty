bdc3c4a090c45ce4e8eb7e7123e462393efc1280
anace <anaclaudia.alfieri@gmail.com>
2019-09-21 19:46:35 +0200

    changing to core's single post navigation

--- template-parts/navigation.php
+++ template-parts/navigation.php
@@ -7,72 +7,14 @@
  * @since 1.0.0
  */
 
-$next_post = get_next_post();
-$prev_post = get_previous_post();
-
-if ( $next_post || $prev_post ) {
-
-	$pagination_classes = '';
-
-	if ( ! $next_post ) {
-		$pagination_classes = ' only-one only-prev';
-	} elseif ( ! $prev_post ) {
-		$pagination_classes = ' only-one only-next';
-	}
-
-	?>
-
-	<nav class="pagination-single section-inner<?php echo esc_attr( $pagination_classes ); ?>" aria-label="<?php esc_attr_e( 'Post', 'twentytwenty' ); ?>">
-
-		<hr class="is-style-wide" aria-hidden="true" />
-
-		<div class="pagination-single-inner">
-
-			<?php
-			if ( $prev_post ) {
-				?>
-
-				<a class="previous-post" href="<?php echo esc_url( get_permalink( $prev_post->ID ) ); ?>">
-					<span class="arrow">&larr;</span>
-					<span class="title"><span class="title-inner"><?php echo wp_kses_post( get_the_title( $prev_post->ID ) ); ?></span></span>
-				</a>
-
-				<?php
-			}
-
-			if ( $next_post ) {
-				?>
-
-				<a class="next-post" href="<?php echo esc_url( get_permalink( $next_post->ID ) ); ?>">
-					<span class="arrow">&rarr;</span>
-						<span class="title"><span class="title-inner"><?php echo wp_kses_post( get_the_title( $next_post->ID ) ); ?></span></span>
-				</a>
-				<?php
-			}
-			?>
-
-		</div><!-- .pagination-single-inner -->
-
-		<hr class="is-style-wide" aria-hidden="true" />
-
-	</nav><!-- .pagination-single -->
-
-	<?php
-}
-?>
-
-<div class="section-inner">
-<hr class="is-style-wide" aria-hidden="true" />
-
-<?php
-
+echo '<hr class="nav-separator is-style-wide" aria-hidden="true" />';
 the_post_navigation(
 	array(
 		'prev_text' => '<span class="arrow" aria-hidden="true">&larr;</span><span class="screen-reader-text">' . __( 'Previous post:', 'twentytwenty' ) . '</span><span class="post-title">%title</span>',
 		'next_text' => '<span class="arrow" aria-hidden="true">&rarr;</span><span class="screen-reader-text">' . __( 'Next post:', 'twentytwenty' ) . '</span><span class="post-title">%title</span>',
 	)
 );
-?>
+echo '<hr class="nav-separator is-style-wide" aria-hidden="true" />';
 
-<hr class="is-style-wide" aria-hidden="true" />
-</div>
+
+
