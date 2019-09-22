9b37ecf21a55202f78e9a4e377e73edc8174c44f
Niels Lange <info@nielslange.de>
2019-09-22 15:12:45 +0200

    UPDATE #91: Fix Travis CI issue

--- functions.php
+++ functions.php
@@ -206,7 +206,7 @@ if ( ! function_exists( 'twentytwenty_register_styles' ) ) {
 			// Add print CSS.
 			wp_enqueue_style( 'twentytwenty-print-style', get_template_directory_uri() . '/print.css', null, $theme_version, 'print' );
 			
-		}
+	}
 
 	add_action( 'wp_footer', 'twentytwenty_register_print_style' );
 
