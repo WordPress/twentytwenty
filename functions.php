1ef9a89fa44506f0a62a021efa5bb43fcc7331a0
Niels Lange <info@nielslange.de>
2019-09-22 11:17:34 +0200

    UPDATE #91: Improve print.css

--- functions.php
+++ functions.php
@@ -200,6 +200,20 @@ if ( ! function_exists( 'twentytwenty_register_styles' ) ) {
 
 	add_action( 'wp_enqueue_scripts', 'twentytwenty_register_styles' );
 
+	/**
+	 * Register and Enqueue Print Styles.
+	 */
+	function twentytwenty_register_print_style() {
+
+			$theme_version = wp_get_theme()->get( 'Version' );
+		
+			// Add print CSS.
+			wp_enqueue_style( 'twentytwenty-print-style', get_template_directory_uri() . '/print.css', null, $theme_version, 'print' );
+			
+		}
+
+	add_action( 'wp_footer', 'twentytwenty_register_print_style' );
+
 }
 
 if ( ! function_exists( 'twentytwenty_register_scripts' ) ) {
