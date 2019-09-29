53fa4e7d990c852f971372b8192ec7eb3bc36a45
Anders <anders@andersnoren.se>
2019-09-29 14:05:48 +0200

    Updated the footer structure

--- footer.php
+++ footer.php
@@ -11,158 +11,53 @@
  * @since 1.0.0
  */
 
-?>
-
+?>		
 		<footer id="site-footer" role="contentinfo" class="header-footer-group">
 
-			<div class="footer-inner section-inner">
+			<div class="footer-bottom section-inner">
 
-				<?php
+				<div class="footer-credits">
 
-				$has_footer_menu = has_nav_menu( 'footer' );
-				$has_social_menu = has_nav_menu( 'social' );
+					<p class="footer-copyright">&copy;
+						<?php
+						echo esc_html(
+							date_i18n(
+								/* Translators: Y = Format parameter for date() https://php.net/manual/en/function.date.php */
+								_x( 'Y', 'Translators: Y = Current year', 'twentytwenty' )
+							)
+						);
+						?>
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo bloginfo( 'name' ); ?></a>
+					</p>
 
-				$footer_top_classes = '';
-
-				$footer_top_classes .= $has_footer_menu ? ' has-footer-menu' : '';
-				$footer_top_classes .= $has_social_menu ? ' has-social-menu' : '';
-
-				$footer_social_wrapper_class = $has_footer_menu ? 'footer-social-wrapper' : '';
-
-				if ( $has_footer_menu || $has_social_menu ) {
-					?>
-					<div class="footer-top<?php echo esc_attr( $footer_top_classes ); ?>">
-						<?php if ( $has_footer_menu ) { ?>
-
-							<nav aria-label="<?php esc_attr_e( 'Footer menu', 'twentytwenty' ); ?>">
-
-								<ul class="footer-menu reset-list-style">
-									<?php
-									wp_nav_menu(
-										array(
-											'container'  => '',
-											'depth'      => 1,
-											'items_wrap' => '%3$s',
-											'theme_location' => 'footer',
-										)
-									);
-									?>
-								</ul>
-
-							</nav><!-- .site-nav -->
-
-						<?php } ?>
-						<?php if ( $has_social_menu ) { ?>
-
-							<div class="<?php echo esc_attr( $footer_social_wrapper_class ); ?>">
-
-								<nav aria-label="<?php esc_attr_e( 'Social links', 'twentytwenty' ); ?>">
-
-									<ul class="social-menu footer-social reset-list-style social-icons s-icons">
-
-										<?php
-										wp_nav_menu(
-											array(
-												'theme_location' => 'social',
-												'container' => '',
-												'container_class' => '',
-												'items_wrap' => '%3$s',
-												'menu_id' => '',
-												'menu_class' => '',
-												'depth'   => 1,
-												'link_before' => '<span class="screen-reader-text">',
-												'link_after' => '</span>',
-												'fallback_cb' => '',
-											)
-										);
-										?>
-
-									</ul>
-
-								</nav><!-- .social-menu -->
-
-							</div><!-- .footer-social-wrapper -->
-
-						<?php } ?>
-					</div><!-- .footer-top -->
-
-				<?php } ?>
-
-
-				<?php if ( is_active_sidebar( 'sidebar-1' ) || is_active_sidebar( 'sidebar-2' ) ) { ?>
-
-					<div class="footer-widgets-outer-wrapper">
-
-						<div class="footer-widgets-wrapper">
-
-							<?php if ( is_active_sidebar( 'sidebar-1' ) ) { ?>
-
-								<div class="footer-widgets column-one grid-item">
-									<?php dynamic_sidebar( 'sidebar-1' ); ?>
-								</div>
-
-							<?php } ?>
-
-							<?php if ( is_active_sidebar( 'sidebar-2' ) ) { ?>
-
-								<div class="footer-widgets column-two grid-item">
-									<?php dynamic_sidebar( 'sidebar-2' ); ?>
-								</div>
-
-							<?php } ?>
-
-						</div><!-- .footer-widgets-wrapper -->
-
-					</div><!-- .footer-widgets-outer-wrapper -->
-
-				<?php } ?>
-
-				<div class="footer-bottom">
-
-					<div class="footer-credits">
-
-						<p class="footer-copyright">&copy;
+					<p class="powered-by-wordpress">
+						<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
 							<?php
-							echo esc_html(
-								date_i18n(
-									/* Translators: Y = Format parameter for date() https://php.net/manual/en/function.date.php */
-									_x( 'Y', 'Translators: Y = Current year', 'twentytwenty' )
-								)
-							);
+							_e( 'Powered by WordPress', 'twentytwenty' ); // phpcs:ignore WordPress.Security.EscapeOutput.UnsafePrintingFunction -- core trusts translations
 							?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo bloginfo( 'name' ); ?></a>
-						</p>
+						</a>
+					</p><!-- .theme-credits -->
 
-						<p class="powered-by-wordpress">
-							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php
-								_e( 'Powered by WordPress', 'twentytwenty' ); // phpcs:ignore WordPress.Security.EscapeOutput.UnsafePrintingFunction -- core trusts translations
-								?>
-							</a>
-						</p><!-- .theme-credits -->
+				</div><!-- .footer-credits -->
 
-					</div><!-- .footer-credits -->
+				<a class="to-the-top" href="#site-header">
+					<span class="to-the-top-long">
+						<?php
+						// Translators: %s = HTML character for an arrow.
+						printf( esc_html( _x( 'To the top %s', '%s = HTML character for an arrow', 'twentytwenty' ) ), '<span class="arrow">&uarr;</span>' );
+						?>
+					</span>
+					<span class="to-the-top-short">
+						<?php
+						// Translators: %s = HTML character for an arrow.
+						printf( esc_html( _x( 'Up %s', '%s = HTML character for an arrow', 'twentytwenty' ) ), '<span class="arrow">&uarr;</span>' );
+						?>
+					</span>
+				</a>
 
-					<a class="to-the-top" href="#site-header">
-						<span class="to-the-top-long">
-							<?php
-							// Translators: %s = HTML character for an arrow.
-							printf( esc_html( _x( 'To the top %s', '%s = HTML character for an arrow', 'twentytwenty' ) ), '<span class="arrow">&uarr;</span>' );
-							?>
-						</span>
-						<span class="to-the-top-short">
-							<?php
-							// Translators: %s = HTML character for an arrow.
-							printf( esc_html( _x( 'Up %s', '%s = HTML character for an arrow', 'twentytwenty' ) ), '<span class="arrow">&uarr;</span>' );
-							?>
-						</span>
-					</a>
+			</div><!-- .footer-bottom -->
 
-				</div><!-- .footer-bottom -->
-
-			</div><!-- .footer-inner -->
-
-		</footer><!-- #site-footer -->
+			</footer><!-- #site-footer -->
 
 		<?php wp_footer(); ?>
 
