1bb6eb5b9107bcc0614c1db57fb1d5f7a2fc5c37
Ian <belangeri79@gmail.com>
2019-10-02 15:36:50 -0400

    Adds header `role="banner"`

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 		wp_body_open();
 		?>
 
-		<header id="site-header" class="header-footer-group">
+		<header id="site-header" class="header-footer-group" role="banner">
 
 			<div class="header-inner section-inner">
 
