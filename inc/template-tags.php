b1dc798813cd99832602e56414ce09f8b84711eb
Anders <anders@andersnoren.se>
2019-10-04 00:00:18 +0200

    Replace wp_unique_id with twentytwenty_unique_id

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -566,6 +566,29 @@ function twentytwenty_get_the_archive_title( $title ) {
 add_filter( 'get_the_archive_title', 'twentytwenty_get_the_archive_title' );
 
 /**
+ * Get unique ID.
+ *
+ * This is a PHP implementation of Underscore's uniqueId method. A static variable
+ * contains an integer that is incremented with each call. This number is returned
+ * with the optional prefix. As such the returned value is not universally unique,
+ * but it is unique across the life of the PHP process.
+ *
+ * @see wp_unique_id() Themes requiring WordPress 5.0.3 and greater should use this instead.
+ *
+ * @staticvar int $id_counter
+ *
+ * @param string $prefix Prefix for the returned ID.
+ * @return string Unique ID.
+ */
+function twentytwenty_unique_id( $prefix = '' ) {
+	static $id_counter = 0;
+	if ( function_exists( 'wp_unique_id' ) ) {
+		return wp_unique_id( $prefix );
+	}
+	return $prefix . (string) ++$id_counter;
+}
+
+/**
  * Filters the edit post link to add an icon and use the post meta structure.
  *
  * @param string $link    Anchor tag for the edit link.
