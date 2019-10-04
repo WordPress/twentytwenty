29b48686a54391a49f00b8f64d2c8530500e99eb
Charlie <carlos@patata.io>
2019-10-04 17:42:46 +0200

    Add backward tab navigation when last link has submenus

--- assets/js/index.js
+++ assets/js/index.js
@@ -421,16 +421,18 @@ twentytwenty.modalMenu = {
 
 			var menuLinks = isDesktop ?
 				document.querySelectorAll( '.menu-modal .expanded-menu .modal-menu li' ) :
-				document.querySelectorAll( '.menu-modal .mobile-menu li' );
+				document.querySelectorAll( '.menu-modal .mobile-menu .modal-menu li' );
 
 			var firstLevelmenuLinks = isDesktop ?
 				document.querySelectorAll( '.menu-modal .expanded-menu .modal-menu > li' ) :
-				document.querySelectorAll( '.menu-modal .mobile-menu > li' );
+				document.querySelectorAll( '.menu-modal .mobile-menu .modal-menu > li' );
 
 			var lastMenuLinkToggleButton = firstLevelmenuLinks[firstLevelmenuLinks.length - 1].querySelector('.sub-menu-toggle') || undefined;
 			var lastMenuLinkHasSubClosedMenu = lastMenuLinkToggleButton && ! lastMenuLinkToggleButton.classList.contains('active');
 
-			var lastToogleSubMenuLinkNotOpened = document.querySelector('.sub-menu .sub-menu-toggle:not(.active)');
+			var lastToogleSubMenuLinkNotOpened =  isDesktop ?
+				document.querySelector( '.menu-modal .expanded-menu .modal-menu .sub-menu .sub-menu-toggle:not(.active)' ) :
+				document.querySelector('.menu-modal .mobile-menu .sub-menu .sub-menu-toggle:not(.active)');
 
 			var socialLinks = document.querySelectorAll( '.menu-modal .social-menu li' );
 			var hasSocialMenu = document.querySelectorAll( '.menu-modal .social-menu' ).length > 0;
@@ -446,11 +448,13 @@ twentytwenty.modalMenu = {
 				focusedElementParentLi[0].className === lastMenuItem.className :
 				undefined;
 
-			if (lastMenuLinkToggleButton && lastMenuLinkHasSubClosedMenu) {
+			if (lastMenuLinkToggleButton && lastMenuLinkHasSubClosedMenu && ! hasSocialMenu) { // Last 1st level item has submenu and is closed
 				isLastModalItem = event.target === lastMenuLinkToggleButton;
+				lastMenuItem = lastMenuLinkToggleButton;
 			}
-			if (lastMenuLinkToggleButton && ! lastMenuLinkHasSubClosedMenu) {
-				isLastModalItem = event.target === lastToogleSubMenuLinkNotOpened || event.target === menuLinks[menuLinks.length - 1].querySelector('a');
+			if (lastMenuLinkToggleButton && ! lastMenuLinkHasSubClosedMenu & ! hasSocialMenu) { // Last 1st level item has submenu is opened
+				isLastModalItem = event.target === lastToogleSubMenuLinkNotOpened || event.target === menuLinks[menuLinks.length - 1].querySelector( 'a' );
+				lastMenuItem = lastToogleSubMenuLinkNotOpened || menuLinks[menuLinks.length - 1].querySelector( 'a' );
 			}
 
 			if ( ! event.shiftKey && event.key === 'Tab' && isLastModalItem ) {
@@ -461,7 +465,11 @@ twentytwenty.modalMenu = {
 			if ( event.shiftKey && event.key === 'Tab' && isFirstModalItem ) {
 				// Backward
 				event.preventDefault();
-				lastMenuItem.querySelector( 'a' ).focus();
+				if (lastMenuItem.querySelector( 'a' )) {
+					lastMenuItem.querySelector( 'a' ).focus()
+				} else {
+					lastMenuItem.focus();
+				}
 			}
 		} );
 	}
