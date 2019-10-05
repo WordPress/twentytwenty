327f6f55f43d586c2c75cbeb942ef47747a27f7b
Anders <anders@andersnoren.se>
2019-10-05 16:45:51 +0200

    Make the two lists of icons filterable

--- classes/class-twentytwenty-svg-icons.php
+++ classes/class-twentytwenty-svg-icons.php
@@ -23,9 +23,23 @@ if ( ! class_exists( 'TwentyTwenty_SVG_Icons' ) ) {
 		 */
 		public static function get_svg( $icon, $group = 'ui', $color = '#1A1A1B' ) {
 			if ( 'ui' === $group ) {
-				$arr = self::$ui_icons;
+				/**
+				 * Filters Twenty Twenty's array of UI icons.
+				 *
+				 * @since Twenty Twenty 1.0.0
+				 *
+				 * @param array $arr Array of UI icons.
+				 */
+				$arr = apply_filters( 'twentytwenty_svg_ui_icons', self::$ui_icons );
 			} elseif ( 'social' === $group ) {
-				$arr = self::$social_icons;
+				/**
+				 * Filters Twenty Twenty's array of social icons.
+				 *
+				 * @since Twenty Twenty 1.0.0
+				 *
+				 * @param array $arr Array of social icons.
+				 */
+				$arr = apply_filters( 'twentytwenty_svg_social_icons', self::$social_icons );
 			} else {
 				$arr = array();
 			}
