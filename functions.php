7f1c008d309ddf6b40e756e00e5bfc76b050492a
Peppa Pig <16564273+ThePigPeppa@users.noreply.github.com>
2019-10-06 19:03:10 -0400

    Optimize Wordpress.

--- functions.php
+++ functions.php
@@ -675,3 +675,32 @@ function twentytwenty_get_elements_array() {
 	*/
 	return apply_filters( 'twentytwenty_get_elements_array', $elements );
 }
+
+/** Remove Query String */
+function _remove_script_version( $src ){
+    $parts = explode( '?ver', $src );
+    return $parts[0];
+}
+
+add_filter( 'script_loader_src', '_remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
+
+/* Minify HTML */
+add_action('get_header', 'gkp_html_minify_start');
+function gkp_html_minify_start()
+{
+    ob_start('gkp_html_minyfy_finish');
+}
+function gkp_html_minyfy_finish($html)
+{
+    $html = preg_replace('/<!--(?!s*(?:[if [^]]+]|!|>))(?:(?!-->).)*-->/s', '', $html);
+    $html = str_replace(array(
+        "\r\n",
+        "\r",
+        "\n",
+        "\t"
+    ), '', $html);
+    while (stristr($html, '  '))
+        $html = str_replace('  ', ' ', $html);
+    return $html;
+}
