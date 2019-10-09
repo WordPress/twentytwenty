427fc8f0b3e4c1b724594bf33b3024624a6c0e3f
Kantari Samy <samy.kantari@gmail.com>
2019-10-09 13:18:40 +0200

    Correct indent
    Signed-off-by: Kantari Samy <samy.kantari@gmail.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -29,8 +29,7 @@ if ( ! Element.prototype.closest ) {
 // https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill
 if ( window.NodeList && ! NodeList.prototype.forEach ) {
 	NodeList.prototype.forEach = function( callback, thisArg ) {
-		var i,
-			len = this.length;
+		var i, len = this.length;
 
 		thisArg = thisArg || window;
 
