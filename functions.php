18db358b2ded9da17b4c023fae6fc7e2868704d4
Anders <anders@andersnoren.se>
2019-10-12 11:24:42 +0200

    Logo HTML filter: Add a style attribute with the height, or append the height to the style attribute if the style attribute already exists.

--- functions.php
+++ functions.php
@@ -261,7 +261,17 @@ function twentytwenty_get_custom_logo( $html ) {
 				"height=\"{$logo_height}\"",
 			);
 
+			// Add a style attribute with the height, or append the height to the style attribute if the style attribute already exists.
+			if ( strpos( $html, ' style=' ) === false ) {
+				$search[]  = '/(src=)/';
+				$replace[] = "style=\"height: {$logo_height}px;\" src=";
+			} else {
+				$search[]  = '/(style="[^"]*)/';
+				$replace[] = "$1 height: {$logo_height}px;";
+			}
+
 			$html = preg_replace( $search, $replace, $html );
+			
 		}
 	}
 
