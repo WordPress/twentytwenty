4c6c165a9371880a85c26c9048057254fab8116b
Mehidi Hassan <mehidihassan258@gmail.com>
2019-10-25 00:01:44 +0600

    removed unused variable

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -38,7 +38,7 @@ function twentytwenty_site_logo( $args = array(), $echo = true ) {
 	$defaults = array(
 		'logo'        => '%1$s<span class="screen-reader-text">%2$s</span>',
 		'logo_class'  => 'site-logo',
-		'title'       => '<a href="%1$s">%2$s</a>',
+		'title'       => '<a href="%1$s" title="%2$s">%2$s</a>',
 		'title_class' => 'site-title',
 		'home_wrap'   => '<h1 class="%1$s">%2$s</h1>',
 		'single_wrap' => '<div class="%1$s faux-heading">%2$s</div>',
